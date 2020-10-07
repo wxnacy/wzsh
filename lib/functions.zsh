34a2683fde4b9f71d8a8f8d4378f3009b18f105e
wxnacy <wxnacy@gmail.com>
2020-10-07 22:16:05 +0800

    Update: 新增 python 版本国内下载方法 pydl

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -14,16 +14,23 @@ function proxyon() {
     echo -e "已开启代理"
 }
 
-# 后台下载视频
-function yd() {
-    nohup youtube-dl $1 &
+# 下载指定版本 Python 包
+function pydl() {
+    v=$1
+    d=$2
+    if [ ! $d ]
+    then
+        d=./
+    fi
+    echo '准备下载 Python' $v
+    curl -L https://npm.taobao.org/mirrors/python/$v/Python-$v.tar.xz -o ${d}/Python-$v.tar.xz
 }
 
 # pyenv 安装 python 新版本
-function pyinstall() {
+function pyi() {
     v=$1
-    echo $v
-    curl https://npm.taobao.org/mirrors/python/$v/Python-$v.tar.xz -o ~/.pyenv/cache/Python-$v.tar.xz
+    echo '准备按照 Python' $v
+    curl -L https://npm.taobao.org/mirrors/python/$v/Python-$v.tar.xz -o ~/.pyenv/cache/Python-$v.tar.xz
     pyenv install $v
 }
 
