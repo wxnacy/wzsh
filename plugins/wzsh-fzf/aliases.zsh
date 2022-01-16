749a471875cc3f8baf4745f60ce9c81b05ba131c
wxnacy <371032668@qq.com>
2022-01-16 16:52:00 +0800

    U: update command f

--- plugins/wzsh-fzf/aliases.zsh
+++ plugins/wzsh-fzf/aliases.zsh
@@ -3,12 +3,13 @@
 # Author: wxnacy <wxnacy@gmail.com>
 # URL: https://wxnacy.com
 # Created: 2022-01-13
-# Modified: 2022-01-13
+# Modified: 2022-01-16
 # Description: fzf 相关改写命令
 #===============================
 
 # 搜索当前目录
-alias f="fzf \
+# 使用 CTRL-T 映射的方法
+alias f="__fsel \
     --preview '(highlight -O ansi {} || cat {}) 2> /dev/null | head -500' \
     --bind 'ctrl-v:execute(vim {})' \
     "
