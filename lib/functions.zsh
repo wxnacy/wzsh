61ddd4e8126a797b72502eb99625531ba0489b53
wxnacy <371032668@qq.com>
2022-04-16 18:25:35 +0800

    F: 完善 plugin create 方法

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -57,7 +57,7 @@ function lnsf() {
 
 function dotall() {
     # 加载全部 dotfile
-    for dotfile in ${WZSH_HOME}/data/dotfile ${HOME}/Documents/Configs/dotfile
+    for dotfile in ${WZSH_HOME}/config/dotfile ${HOME}/Documents/Configs/dotfile
     do
         if [[ -f $dotfile ]]
         then
