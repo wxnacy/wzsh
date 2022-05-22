40772232e3697a0100ad8fdde916a71cb185d98b
wxnacy <371032668@qq.com>
2022-05-22 17:05:56 +0800

    add plugin wzsh-go

---
+++ plugins/wzsh-go/wzsh-go.zsh
@@ -0,0 +1,5 @@
+. ${WZSH_HOME}/plugins/wzsh-go/aliases.zsh
+. ${WZSH_HOME}/plugins/wzsh-go/zprofile
+. ${WZSH_HOME}/plugins/wzsh-go/zshenv
+. ${WZSH_HOME}/plugins/wzsh-go/zshrc
+. ${WZSH_HOME}/plugins/wzsh-go/zprofile
