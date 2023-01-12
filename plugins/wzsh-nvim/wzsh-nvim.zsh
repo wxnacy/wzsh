acf5e29def2ee822f452d1a579ee5a18d7eeb0c3
wxnacy <371032668@qq.com>
2023-01-12 15:45:23 +0800

    规范创建插件方法

--- plugins/wzsh-nvim/wzsh-nvim.zsh
+++
@@ -1,5 +0,0 @@
-. ${WZSH_HOME}/plugins/wzsh-nvim/aliases.zsh
-. ${WZSH_HOME}/plugins/wzsh-nvim/zprofile
-. ${WZSH_HOME}/plugins/wzsh-nvim/zshenv
-. ${WZSH_HOME}/plugins/wzsh-nvim/zshrc
-. ${WZSH_HOME}/plugins/wzsh-nvim/zprofile
--- plugins/wzsh-nvm/wzsh-nvm.zsh
+++
@@ -1,1 +0,0 @@
-. ${WZSH_HOME}/plugins/wzsh-nvm/aliases.zsh
