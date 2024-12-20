c506ea918d4aab29eb2298cdacbea0d95503cff4
wxnacy <wxnacy@gmail.com>
2024-12-20 21:44:22 +0800

    feat(plugins): add smart-enter

--- plugins/wzsh-yazi/config/init.lua
+++ plugins/wzsh-yazi/config/init.lua
@@ -1,3 +1,7 @@
 -- plugins config
 -- https://github.com/yazi-rs/plugins/tree/main/git.yazi
 require("git"):setup()
+-- https://github.com/yazi-rs/plugins/tree/main/smart-enter.yazi
+require("smart-enter"):setup {
+	open_multi = true,
+}
