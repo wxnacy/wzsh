44cf97a8b37057828e8a139eb2110006c4d615ba
wxnacy <wxnacy@gmail.com>
2024-12-29 20:03:11 +0800

    feat(wzsh-yazi): 增加 yamb 注释信息

--- plugins/wzsh-yazi/config/init.lua
+++ plugins/wzsh-yazi/config/init.lua
@@ -9,3 +9,15 @@ require("smart-enter"):setup {
 require("full-border"):setup()
 -- https://github.com/Rolv-Apneseth/starship.yazi?tab=readme-ov-file
 require("starship"):setup()
+-- https://github.com/h-hg/yamb.yazi
+-- local bookmarks = {}
+-- require("yamb"):setup {
+  -- -- Optional, the path ending with path seperator represents folder.
+  -- bookmarks = bookmarks,
+  -- -- Optional, recieve notification everytime you jump.
+  -- jump_notify = true,
+  -- -- Optional, the cli of fzf.
+  -- cli = "fzf",
+  -- -- Optional, the path of bookmarks
+  -- path = (os.getenv("HOME") .. "/Documents/Configs/wzsh/yazi/bookmarks"),
+-- }
