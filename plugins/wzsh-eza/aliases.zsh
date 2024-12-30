29a51e5faf1a2eee3963c73cc329e2286883f43b
wxnacy <wxnacy@gmail.com>
2024-12-30 20:11:59 +0800

    feat(wzsh-eza): add lt

--- plugins/wzsh-eza/aliases.zsh
+++ plugins/wzsh-eza/aliases.zsh
@@ -1,4 +1,5 @@
 default_arg="--icons=always -olMh --git --git-repos-no-status --time-style='+%Y-%m-%d %H:%M'"
 alias ll="eza ${default_arg}"
 alias la="eza -a ${default_arg}"
-alias lt="eza -a --total-size ${default_arg}"
+alias lh="eza -a --total-size ${default_arg}"
+alias lt="eza -a -T -L 3 ${default_arg}"
