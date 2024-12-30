50307f71108a499651d6643c3375e1544a6f969f
wxnacy <wxnacy@gmail.com>
2024-12-30 09:45:58 +0800

    feat(wzsh-eza): 补充 aliases

--- plugins/wzsh-eza/aliases.zsh
+++ plugins/wzsh-eza/aliases.zsh
@@ -1,2 +1,4 @@
-alias ll="eza -l --icons=always"
-alias la="eza -la --icons=always"
+default_arg="--icons=always -olMh --git --git-repos-no-status --time-style='+%Y-%m-%d %H:%M'"
+alias ll="eza ${default_arg}"
+alias la="eza -a ${default_arg}"
+alias lt="eza -a --total-size ${default_arg}"
