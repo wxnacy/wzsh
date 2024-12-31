eddc0c11607d997ff97f0c2586839411c59889c4
wxnacy <wxnacy@gmail.com>
2024-12-31 22:02:07 +0800

    feat(wzsh-yazi): add plugin yamb

--- plugins/wzsh-yazi/config/init.lua
+++ plugins/wzsh-yazi/config/init.lua
@@ -10,14 +10,18 @@ require("full-border"):setup()
 -- https://github.com/Rolv-Apneseth/starship.yazi?tab=readme-ov-file
 require("starship"):setup()
 -- https://github.com/h-hg/yamb.yazi
--- local bookmarks = {}
--- require("yamb"):setup {
-  -- -- Optional, the path ending with path seperator represents folder.
-  -- bookmarks = bookmarks,
-  -- -- Optional, recieve notification everytime you jump.
-  -- jump_notify = true,
-  -- -- Optional, the cli of fzf.
-  -- cli = "fzf",
-  -- -- Optional, the path of bookmarks
-  -- path = (os.getenv("HOME") .. "/Documents/Configs/wzsh/yazi/bookmarks"),
--- }
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+require("yamb"):setup {
+  -- Optional, the path ending with path seperator represents folder.
+  bookmarks = bookmarks,
+  -- Optional, recieve notification everytime you jump.
+  jump_notify = true,
+  -- Optional, the cli of fzf.
+  cli = "fzf",
+  -- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+  keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  -- Optional, the path of bookmarks
+  path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark") or
+        (os.getenv("HOME") .. "/Documents/Configs/wzsh/yazi/yamb.bookmark"),
+}
