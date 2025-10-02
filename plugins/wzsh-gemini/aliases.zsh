195280dc349c9f019caf45d66b8440ba98aece01
wxnacy <wxnacy@gmail.com>
2025-10-02 15:29:26 +0800

    feat(plugins): add gemini

---
+++ plugins/wzsh-gemini/aliases.zsh
@@ -0,0 +1,2 @@
+# https://github.com/google-gemini/gemini-cli/blob/main/docs/cli/configuration.md
+alias gemi="gemini --proxy=${PROXY} --approval-mode=auto_edit"
---
+++ plugins/wzsh-gemini/config/GEMINI.md
@@ -0,0 +1,2 @@
+## Gemini Added Memories
+- 我应该一直使用中文来回复用户
