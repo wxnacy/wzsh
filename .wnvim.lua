dc019137099d17eb5de5424d75ee01daf95c4c89
wxnacy <wxnacy@gmail.com>
2025-11-14 10:36:49 +0800

    chore(nvim): add .wnvim.lua

---
+++ .wnvim.lua
@@ -0,0 +1,13 @@
+return {
+	filetype = {
+		sh = {
+			pattern = { "*.envrc" },
+		},
+		zsh = {
+			pattern = { "zprofile", "installer" },
+		},
+		ruby = {
+			pattern = { "Brewfile" },
+		},
+	},
+}
