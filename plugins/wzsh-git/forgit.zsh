cf14ca9ab03edd95220d43f826a40f920318f50d
wxnacy <wxnacy@gmail.com>
2025-11-20 19:23:14 +0800

    feat(git): add forgit

---
+++ plugins/wzsh-git/forgit.zsh
@@ -0,0 +1,17 @@
+# https://github.com/wfxr/forgit
+export FORGIT_FZF_DEFAULT_OPTS="
+$FZF_DEFAULT_OPTS
+--ansi
+--height='80%'
+--bind='alt-k:preview-up,alt-p:preview-up'
+--bind='alt-j:preview-down,alt-n:preview-down'
+--bind='ctrl-r:toggle-all'
+--bind='ctrl-s:toggle-sort'
+--bind='?:toggle-preview'
+--bind='alt-w:toggle-preview-wrap'
+--preview-window='right:60%'
++1
+$FORGIT_FZF_DEFAULT_OPTS
+"
+zinit ice lucid wait='2'
+zinit light wfxr/forgit
