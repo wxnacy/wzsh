d7b2181922b2113714048957b4181c048a97c0a4
wxnacy <wxnacy@gmail.com>
2026-03-15 12:55:52 +0800

    fix(yazi): O 键直接使用 open --interactive，移除 smart-open 插件

--- plugins/wzsh-yazi/config/plugins/smart-open.yazi/main.lua
+++
@@ -1,9 +0,0 @@
---- @since 26.1.22
---- @sync entry
--- smart-open: 不管目录还是文件，都使用 open --interactive 交互式打开
-
-local function entry()
-	ya.emit("open", { interactive = true })
-end
-
-return { entry = entry }
