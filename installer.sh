4fc67a360cfeeb04d9655565db82931ef02cb6d8
wxnacy <wxnacy@gmail.com>
2026-04-03 16:26:54 +0800

    chore(installer): 改变下载地址

--- installer.sh
+++ installer.sh
@@ -18,44 +18,44 @@ if [ ${#missing[@]} -ne 0 ]; then
         . /etc/os-release
     fi
     case "${system}" in
-        Darwin)
-            echo "macOS 安装命令: brew install ${missing[*]}"
+    Darwin)
+        echo "macOS 安装命令: brew install ${missing[*]}"
+        ;;
+    Linux)
+        case "${ID:-}" in
+        ubuntu | debian)
+            echo "Ubuntu/Debian 安装命令: sudo apt update && sudo apt install -y ${missing[*]}"
             ;;
-        Linux)
-            case "${ID:-}" in
-                ubuntu|debian)
-                    echo "Ubuntu/Debian 安装命令: sudo apt update && sudo apt install -y ${missing[*]}"
-                    ;;
-                centos|rhel)
-                    echo "CentOS/RHEL 安装命令: sudo yum install -y ${missing[*]}"
-                    ;;
-                fedora)
-                    echo "Fedora 安装命令: sudo dnf install -y ${missing[*]}"
-                    ;;
-                arch|manjaro)
-                    echo "Arch/Manjaro 安装命令: sudo pacman -S --needed ${missing[*]}"
-                    ;;
-                opensuse-leap|opensuse-tumbleweed|sles)
-                    echo "openSUSE/SLES 安装命令: sudo zypper install -y ${missing[*]}"
-                    ;;
-                *)
-                    echo "通用 Linux 安装命令: 使用发行版的软件包管理器安装 ${missing[*]}"
-                    ;;
-            esac
+        centos | rhel)
+            echo "CentOS/RHEL 安装命令: sudo yum install -y ${missing[*]}"
+            ;;
+        fedora)
+            echo "Fedora 安装命令: sudo dnf install -y ${missing[*]}"
+            ;;
+        arch | manjaro)
+            echo "Arch/Manjaro 安装命令: sudo pacman -S --needed ${missing[*]}"
+            ;;
+        opensuse-leap | opensuse-tumbleweed | sles)
+            echo "openSUSE/SLES 安装命令: sudo zypper install -y ${missing[*]}"
             ;;
         *)
-            echo "请使用系统的软件包管理器安装: ${missing[*]}"
+            echo "通用 Linux 安装命令: 使用发行版的软件包管理器安装 ${missing[*]}"
             ;;
+        esac
+        ;;
+    *)
+        echo "请使用系统的软件包管理器安装: ${missing[*]}"
+        ;;
     esac
     exit 1
 fi
 
 CUR_DIR=$(pwd)
-W_HOME=$HOME/.local/share/wzsh
-W_GIT=$W_HOME/wzsh.git
-command mkdir -p "$W_HOME" && command chmod g-rwX "$W_HOME"
+W_HOME=$HOME/.wzsh
+W_GIT=$W_HOME
+# command mkdir -p "$W_HOME" && command chmod g-rwX "$W_HOME"
 test -d "$W_GIT" || git clone --recursive --depth=1 https://github.com/wxnacy/wzsh "${W_GIT}"
 cd $W_GIT
+echo "Begin Install in: $(pwd)"
 make install
 cd $CUR_DIR
-
