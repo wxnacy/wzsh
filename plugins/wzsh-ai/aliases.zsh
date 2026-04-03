77039c6c1ea7dd848076b1aa6eb8282746696c8b
wxnacy <wxnacy@gmail.com>
2026-04-03 20:47:37 +0800

    Backup on 2026-04-03 20:47:37

--- plugins/wzsh-ai/aliases.zsh
+++
@@ -1,4 +0,0 @@
-# for ollama begin
-alias ol="ollama"
-# alias agent="/Users/wxnacy/Projects/agent/.venv/bin/agent"
-# for ollama end
--- plugins/wzsh-gemini/aliases.zsh
+++
@@ -1,2 +0,0 @@
-# https://github.com/google-gemini/gemini-cli/blob/main/docs/cli/configuration.md
-# alias gemi="gemini --proxy=${PROXY} --approval-mode=auto_edit"
