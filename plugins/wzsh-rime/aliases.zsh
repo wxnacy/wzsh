1d0ddb7cffaa93889a22c81d50f602fbfe6207a0
wxnacy <wxnacy@gmail.com>
2026-04-27 18:14:08 +0800

    feat(plugins): add rime

---
+++ plugins/wzsh-rime/aliases.zsh
@@ -0,0 +1,2 @@
+alias rime-ice-install="proxy on; cd /Users/wxnacy/.local/share/rime/plum; ./rime-install iDvel/rime-ice:others/recipes/full; cd -; proxy off"
+alias rime-reload="/Library/Input\ Methods/Squirrel.app/Contents/MacOS/Squirrel --reload"
